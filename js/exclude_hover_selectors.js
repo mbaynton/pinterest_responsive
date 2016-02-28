5a16f9a8718b63aff2b526c7d1e6dd8e186077cf
Michael Baynton <mike@mbaynton.com>
2016-02-28 10:50:41 -0600

    Rename module from pinterest_responsive to pinterest_hover, pull in pinit.js by default out-of-the-box, add generic image hoverbutton capabilities & configurable CSS selector exclude.

---
+++ js/exclude_hover_selectors.js
@@ -0,0 +1,14 @@
+(function ($) {
+    Drupal.behaviors.pinterest_hover = {
+        attach: function (context, settings) {
+            var excluded = settings.pinterest_hover.excluded;
+
+            // Disable pinning items specified by the selectors
+            for (var i = 0; i < excluded.length; i++) {
+                // pinit_main.js won't add hoverbuttons to anything but img
+                // https://github.com/pinterest/widgets/blob/ae255301c66864a55bd8ba2c750d60e7ffefc842/pinit_main.js#L786
+                $(excluded[i]).filter('img').attr('data-pin-nopin', '1');
+            }
+        }
+    };
+})(jQuery);
